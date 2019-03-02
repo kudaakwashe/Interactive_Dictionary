0a7c06e140f6a62cc70a149a0aaa7aacecb4734c
kudaakwashe <kudaakwashe@gmail.com>
2019-03-02 22:54:36 +0200

    fixing bugs

--- app1.py
+++ app1.py
@@ -7,20 +7,34 @@ def translate(word):
     word = word.lower()
     if word in data:
         return data[word]
+
+    elif word.title() in data:
+        return data[word.title()]
+
+    elif word.upper() in data:
+        return data[word.upper()]
+
     elif len(get_close_matches(word, data.keys())) > 0:
-        yn =input("Did you mean %s instead? Enter Y if yes, or N if no: " % get_close_matches(word, data.keys(), n=3, cutoff=0.7)[0])
+        yn =input("Did you mean %s instead? Enter Y if yes, or N if no: " % get_close_matches(word, data.keys(), n=3, cutoff=0.6)[0])
         yn = yn.lower()
         if yn == "y":
             return data[get_close_matches(word, data.keys())[0]]
         elif yn == "n":
-            return input("The word doesn't exist. Please double check it."
+            return "The word doesn't exist. Please double check it."
         else:
             return "We didnt understand your entry."
+
     elif len(get_close_matches(word, data.keys())) == 0:
         return "The word doesn't exist. Please double check it."
+        
     else:
         return "The word doesn't exist. Please double check it."
 
 word = input("Enter word: ")
 
-print(translate(word))
+output = translate(word)
+if type(output) == list:
+    for item in output:
+        print(item)
+else:
+    print(output)
